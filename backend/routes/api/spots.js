2bb096615d7512fa9d171d23f68af0ddea27c0be
cludwell <cludwell@gmail.com>
2023-02-21 12:44:18 -0800

    working on spots router

---
+++ backend/routes/api/spots.js
@@ -0,0 +1,31 @@
+const express = require('express')
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User, Spot } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const router = express.Router();
+const {sequelize, Op} = require('sequelize')
+
+router.get('/', async (req, res, next) => {
+    let {page, size} = req.query, pagination = {};
+
+    !size || parseInt(size) <= 0 ? size = 20
+    : size = parseInt(size)
+
+    !page || parseInt(page) <= 0 ? page = 1
+    : page = parseInt(page)
+
+    if (page >=1 && size >= 1){
+      pagination.limit = size;
+      pagination.offset = size * (page - 1)
+    }
+
+    const spots = await Spot.findAll({
+        ...pagination
+    })
+
+    res.status(200).json(spots)
+})
+
+module.exports = router;
