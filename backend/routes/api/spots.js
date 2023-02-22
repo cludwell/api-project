e9754f84b94f16972282fe610339552b2dc20b4d
cludwell <cludwell@gmail.com>
2023-02-21 23:18:39 -0800

    did query validation errors for find all spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -8,11 +8,25 @@ const router = express.Router();
 const {sequelize, Op} = require('sequelize');
 const spot = require('../../db/models/spot');
 const user = require('../../db/models/user');
+const csrf = require('csurf');
+const cookieParser = require('cookie-parser');
+router.use(cookieParser())
 
+
+router.use(
+    // configs the app to use the cookies
+  csrf({
+    cookie: true
+  })
+);
+
+router.use((req, res, next)=> {
+    res.cookie('XSRF-TOKEN', req.csrfToken())
+})
 //post a spot
 router.post('/', async (req, res, next) => {
     if (!req.user.id) res.status(400).json({message: 'Please sign in to post a spot'})
-    res.cookie('XSRF-TOKEN', req.csrfToken)
+
     let newSpot = await Spot.create({
         ownerId: req.user.id,
         address,
@@ -61,39 +75,37 @@ router.get('/:id', async (req, res, next) => {
 
 //get a list of all spots
 router.get('/', async (req, res, next) => {
-    let {page, size} = req.query, pagination = {};let where = {}
+    let {page, size, minPrice, maxPrice, minLat, maxLat, minLng, maxLng} = req.query
+    let where = {}, errors = [], pagination = {}
 
-    size = !size || parseInt(size) <= 0 || parseInt(size) > 20 ? size = 20
+    size = !size || parseInt(size) > 20 ? size = 20
     : parseInt(size)
 
-    page = !page || parseInt(page) <= 0 ? page = 0
+    page = !page ? page = 0
     : parseInt(page) > 10 ? 10
     : parseInt(page)
 
+    if (size < 0) errors.push("Size must be greater than or equal to 0",)
+    if(page < 0) errors.push("Page must be greater than or equal to 0",)
     if (page >=1 && size >= 1){
         pagination.limit = size;
         pagination.offset = size * (page - 1)
     }
 
     //accepting query parameters
-    if (req.query.minPrice >= 0) {
-        where.price = { [Op.gte]: req.query.minPrice }
-    }
-    if (req.query.maxPrice >= 0) {
-        where.price = { [Op.lte]: req.query.maxPrice }
-    }
-    if (req.query.minLat) {
-        where.lat = { [Op.gte]: req.query.minLat }
-    }
-    if (req.query.maxLat) {
-        where.lat = { [Op.lte]: req.query.maxLat }
-    }
-    if (req.query.minLng) {
-        where.lng = { [Op.gte]: req.query.maxPrice }
-    }
-    if (req.query.maxLng) {
-        where.lng = { [Op.lte]: req.query.maxPrice }
-    }
+    if (minPrice >= 0) where.price = { [Op.gte]: minPrice }
+    if (minPrice < 0) errors.push("Minimum price must be greater than or equal to 0")
+    if (maxPrice >= 0) where.price = { [Op.lte]: maxPrice }
+    if (maxPrice < 0) errors.push("Maximum price must be greater than or equal to 0")
+    if (minLat <= 180 || minLat >= 180 ) where.lat = { [Op.gte]: minLat }
+    if (minLat > 180 || minLat < -180 || maxLat < minLat) errors.push("Minimum latitude is invalid")
+    if (maxLat <= 180 || maxLat >= -180) where.lat = { [Op.lte]: maxLat }
+    if (maxLat > 180 || maxLat < -180 || maxLat < minLat) errors.push("Maximum latitude is invalid")
+    if (minLng <= 180 || minLng >= -180) where.lng = { [Op.gte]: maxPrice }
+    if (minLng > 180 || minLng < -180 || minLng > maxLng) errors.push("Minimum longitude is invalid")
+    if (maxLng <= 180 || maxLng <= -180) where.lng = { [Op.lte]: maxPrice }
+    if (maxLng > 180 || maxLng < -180 || maxLng < minLng) errors.push("Minimum longitude is invalid")
+
     let Spots = []
     const spotsData = await Spot.findAll( {
         where,
@@ -115,6 +127,11 @@ router.get('/', async (req, res, next) => {
         payload.previewImage = previewImageData.url
         Spots.push(payload)
     }
+    if (errors.length) res.status(400).json({
+        "message": "Validation Error",
+        "statusCode": 400,
+        "errors": errors
+    })
     res.status(200).json({Spots: Spots, page: page + 1, size})
 })
 
