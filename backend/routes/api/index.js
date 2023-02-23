ac5375486a10bf688d4854cfe2cad306e09a4072
cludwell <cludwell@gmail.com>
2023-02-23 15:39:34 -0800

    created a router for review images

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,7 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const { restoreUser } = require("../../utils/auth.js");
 const reviewsRouter = require('./reviews')
+const reviewImageRouter = require ('./review-images')
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
   // If current user session is not valid, set req.user to null
@@ -12,6 +13,7 @@ router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 router.use('/reviews', reviewsRouter);
+router.use('/review-images', reviewImageRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
---
+++ backend/routes/api/review-images.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const { User, Spot, SpotImage, Review, ReviewImage } = require('../../db/models');
+const { check, Result } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+const router = express.Router();
+const {sequelize, Op} = require('sequelize');
+const csrf = require('csurf');
+const cookieParser = require('cookie-parser');
+router.use(cookieParser())
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth')
+
+
+
+
+router.use((err, _req, _res, next) => {
+    const error = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = { message: "The requested resource couldn't be found." };
+    err.status = 404;
+    next(err);
+  });
+
+module.exports = router;
