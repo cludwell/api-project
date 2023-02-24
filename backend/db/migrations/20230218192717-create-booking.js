f3c488fe8f3ffab3c43eeb1d76bf4af25214dd11
cludwell <cludwell@gmail.com>
2023-02-24 15:06:46 -0800

    options

--- backend/db/migrations/20230218192717-create-booking.js
+++ backend/db/migrations/20230218192717-create-booking.js
@@ -45,6 +45,6 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     options.tableName = 'Bookings'
-    await queryInterface.dropTable('Bookings');
+    await queryInterface.dropTable(options);
   }
 };
