3faf29f619c0f1c2d0cddb28adeb313eec484b22
cludwell <cludwell@gmail.com>
2023-02-23 16:44:49 -0800

    started creating a bookings router and routes

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,7 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const { restoreUser } = require("../../utils/auth.js");
 const spotsRouter = require('./spots.js')
+const bookingsRouter = require('./bookings')
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
   // If current user session is not valid, set req.user to null
@@ -13,6 +14,12 @@ router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 
 router.use('/spots', spotsRouter);
+
+
+router.use('/spots', spotsRouter);
+
+router.use('./bookings', bookingsRouter)
+
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
