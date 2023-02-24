e0abb9c89ee180a0011e411e6320af0f866b516e
cludwell <cludwell@gmail.com>
2023-02-24 10:18:15 -0800

    finished edit a booking but requires testing

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -36,7 +36,74 @@ router.get('/current', restoreUser, async (req, res) => {
 
 //Edit a Booking
 router.put('/:bookingId', restoreUser, async (req, res) => {
+    let {startDate, endDate} = req.body
+    let bookingQuery = await Booking.findByPk(req.params.bookingId)
+    if (!bookingQuery) {
+        res.status(404).json({
+      "message": "Booking couldn't be found",
+      "statusCode": 404
+    })
+    }
+    if (Date.parse(bookingQuery.endDate) < new Date()) {
+        res.status(403).json({
+      "message": "Past bookings can't be modified",
+      "statusCode": 403
+    })
+    }
 
+    let spot = await Spot.findOne({
+        where: {id: bookingQuery.spotId}
+    })
+
+
+
+    if (Date.parse(endDate) <= Date.parse(startDate)) {
+        res.status(400).json({
+            "message": "Validation error",
+            "statusCode": 400,
+            "errors": {
+              "endDate": "endDate cannot be on or before startDate"
+            }
+          })
+    }
+
+    let bookings = await Booking.findAll({
+        where: {spotId: spot.id},
+        attributes: ['startDate', 'endDate']
+    })
+
+    let bookingsArray = [], errors = {}
+    for (let book of bookings) {
+        bookingsArray.push([Date.parse(book.startDate), Date.parse(book.endDate)])
+    }
+    let parsedStart = Date.parse(startDate), parsedEnd = Date.parse(endDate)
+
+    let startingConflicts = bookingsArray.sort((a,b) => a[0] - b[0])
+        .filter(ele=> (ele[0] <= parsedStart && parsedStart <= ele[1]))
+    let endingConflicts = bookingsArray.sort((a,b) => a[0] - b[0])
+        .filter(ele=> (ele[0] <= parsedEnd && parsedEnd <= ele[1]))
+
+    if (startingConflicts.length) {
+        errors.startDate = "Start date conflicts with an existing booking"
+    }
+    if (endingConflicts.length) {
+        errors.endDate = "End date conflicts with an existing booking"
+    }
+    if (startingConflicts.length || endingConflicts.length) {
+        res.status(403).json({
+            "title": "Booking conflict",
+            "message": "Sorry, this spot is already booked for the specified dates",
+            "statusCode": 403,
+            errors
+        })
+    }
+
+    let bookingEdit = bookingQuery.set({
+        "startDate": startDate,
+        "endDate": endDate
+    })
+    await bookingEdit.save()
+    res.status(200).json(bookingEdit)
 })
 
 //Delete a Booking
