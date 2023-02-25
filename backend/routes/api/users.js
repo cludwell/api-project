649dad179a8c6789a2ff9c90ddbaab1d7dc00681
cludwell <cludwell@gmail.com>
2023-02-24 16:47:28 -0800

    fixed sign up route

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -30,62 +30,15 @@ const validateSignup = [
 
 
 // Sign up
-router.post('/', async (req, res) => {
-    const errors = {}
+router.post('/', validateSignup, async (req, res) => {
+  
     const { email, password, username, firstName, lastName } = req.body;
     const user = await User.signup({ email, username, password, firstName, lastName});
     let token = await setTokenCookie(res, user);
 
-    if (!email) errors.email = 'Invalid email'
-    if (!username) errors.username = 'Username is required'
-    if (!firstName) errors.firstName = 'First Name is required'
-    if (!lastName) errors.lastName ='Last Name is required'
-    if (!password) errors.password = 'Password is required'
-
-    //conditionfor if user email exists
-    const userEmail = await User.findOne({
-      where: {email: email}
-    })
-    const userUsername = await User.findOne({
-      where: {username: username}
-    })
-
-    if (userEmail) {
-      let error = {
-          title: 'User already exists with the specified email',
-          message: "User already exists",
-          statusCode: 403,
-          errors: {
-          email: 'User with that email already exists'
-          }
-        }
-      res.status(403).json(error)
-    }
-
-    //condition for if user username exists
-    if (userUsername) {
-      let error = {
-        title: 'User already exists with the specified username',
-        message: "User already exists",
-        statusCode: 403,
-        errors: {
-        email: 'User with that username already exists'
-        }
-      }
-    res.status(403).json(error)
-    }
-
-    if (Object.keys(errors).length) {
-      res.status(400).json({
-        "message": "Validation error",
-        "statusCode": 400,
-        errors
-      })
-    }
-
     res.json({
+      id: user.id,
       email: email,
-      password: password,
       username: username,
       firstName: firstName,
       lastName: lastName,
