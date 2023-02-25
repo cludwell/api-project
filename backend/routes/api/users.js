db39c149f28bda17584d202f9d49e2dc2ded60bf
cludwell <cludwell@gmail.com>
2023-02-24 16:57:04 -0800

    refactoring sign up

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -30,21 +30,68 @@ const validateSignup = [
 
 
 // Sign up
-router.post('/', validateSignup, async (req, res) => {
-  
+router.post('/', async (req, res) => {
+    const errors = {}
     const { email, password, username, firstName, lastName } = req.body;
-    const user = await User.signup({ email, username, password, firstName, lastName});
-    let token = await setTokenCookie(res, user);
 
-    res.json({
-      id: user.id,
-      email: email,
-      username: username,
-      firstName: firstName,
-      lastName: lastName,
-      token: token});
-  }
-);
+    if (!email) errors.email = 'Invalid email'
+    if (!username) errors.username = 'Username is required'
+    if (!firstName) errors.firstName = 'First Name is required'
+    if (!lastName) errors.lastName ='Last Name is required'
+    if (!password) errors.password = 'Password is required'
+
+    // condition for if user email exists
+    const userEmail = await User.findOne({
+      where: {email: email}
+    })
+    const userUsername = await User.findOne({
+      where: {username: username}
+    })
+
+    if (userEmail) {
+      let error = {
+        title: 'User already exists with the specified email',
+        message: "User already exists",
+        statusCode: 403,
+        errors: {
+          email: 'User with that email already exists'
+          }
+        }
+        res.status(403).json(error)
+      }
+
+      //condition for if user username exists
+      if (userUsername) {
+        let error = {
+          title: 'User already exists with the specified username',
+          message: "User already exists",
+          statusCode: 403,
+          errors: {
+            email: 'User with that username already exists'
+          }
+        }
+        res.status(403).json(error)
+      }
+
+      if (Object.keys(errors).length) {
+        res.status(400).json({
+          "message": "Validation error",
+          "statusCode": 400,
+          errors
+        })
+      }
+      const user = await User.signup({ email, username, password, firstName, lastName});
+      let token = await setTokenCookie(res, user);
+
+      res.json({
+        email: email,
+        password: password,
+        username: username,
+        firstName: firstName,
+        lastName: lastName,
+        token: token});
+      }
+      );
 
 
 module.exports = router;
