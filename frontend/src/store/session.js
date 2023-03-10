1afa32763445c40dd782df83b1d4496d4373b54c
cludwell <cludwell@gmail.com>
2023-03-10 14:24:26 -0800

    front authme phase3

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -30,7 +30,6 @@ export const login = (user) => async (dispatch) => {
   dispatch(setUser(data.user));
   return response;
 };
-
 export const restoreUser = () => async dispatch => {
     const response = await csrfFetch('/api/session');
     const data = await response.json();
@@ -54,7 +53,13 @@ export const signup = (user) => async (dispatch) => {
   dispatch(setUser(data.user));
   return response;
 };
-
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+      method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+  };
 
 const initialState = { user: null };
 
