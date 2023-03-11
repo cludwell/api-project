2a59fce2a27cb3caefc69312341fcb97dfa5c286
cludwell <cludwell@gmail.com>
2023-03-11 13:59:13 -0800

    favicon, title, header

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,19 +1,25 @@
 import Navigation from '../Navigation'
+import ProfileButton from '../Navigation/ProfileButton'
 import './Header.css'
+import logo from '../../images/purpplanchette.png'
+import { Link } from 'react-router-dom'
 
 function Header({ props }) {
     return (
         <>
         <div className='header'>
-            <div className='logo-side'>
-            <i class="fa-solid fa-ghost"></i>
-            <span className='logo'>scarebnb</span>
-            </div>
 
+        <div className='logo-side'>
+            <Link to={`/`}>
+            <img src={logo} alt='logo' className='planchette'/>
+            {/* <i class="fa-solid fa-ghost"></i> */}
+            <span className='logo'>scarebnb</span>
+            </Link>
+        </div>
             <div className='navigation-corner'>
                 <span className='scarebnb-home'>Scarebnb your home</span>
                 <i class="fa-solid fa-globe"></i>
-                <Navigation></Navigation>
+                <ProfileButton></ProfileButton>
             </div>
         </div>
         </>
