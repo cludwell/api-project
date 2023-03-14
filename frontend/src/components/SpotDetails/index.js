146e0789dbc02e27cac2d8ddb1aceea306009617
cludwell <cludwell@gmail.com>
2023-03-14 12:52:59 -0700

    removed error for keys in jsx eles

--- frontend/src/components/SpotDetails/index.js
+++ frontend/src/components/SpotDetails/index.js
@@ -11,6 +11,7 @@ export default function SpotDetails() {
     useEffect(() => {
         dispatch(findSingleSpot(spotId))
         dispatch(findSpotReviews(spotId))
+        // dispatch()
     }, [dispatch, spotId])
     const singleSpot = useSelector(state => state.singleSpot)
     const spotReviews = useSelector(state => state.reviews)
@@ -19,17 +20,7 @@ export default function SpotDetails() {
     if (!Object.entries(singleSpot).length) return null;
     if (!Object.entries(spotReviews).length) return null;
 
-    const reviewStats = singleSpot.numReviews === 0 ? (<>
-            <i className="fa-solid fa-star"></i>
-            New!
-            </>) : (<>
-            <i className="fa-solid fa-star"></i>
-            {singleSpot.avgStarRating}
-             ●
-            {singleSpot.numReviews === 1 ? singleSpot.numReviews + ' Review'
-            :singleSpot.numReviews + ' Reviews'}
-        </>)
-        
+    const featureAlert = () => alert('Feature coming soon')
     return (
     <>
         <div className='spot-details'>
@@ -49,20 +40,42 @@ export default function SpotDetails() {
             <p className='description'>{singleSpot.description}</p>
             <div className='price-review-reserve'>
                 <span className='reserve-left'>${singleSpot.price} night</span>
-                <span className='reserve-right'>{reviewStats}</span>
-                <button className='reserve-button'>Reserve</button>
+                <span className='reserve-right'>
+            {singleSpot.numReviews === 0 ? (<>
+            <i className="fa-solid fa-star"></i>
+            New!
+            </>) : (<>
+            <i className="fa-solid fa-star"></i>
+            {singleSpot.avgStarRating}
+             ●
+            {singleSpot.numReviews === 1 ? singleSpot.numReviews + ' Review'
+            :singleSpot.numReviews + ' Reviews'}
+        </>)}
+                </span>
+                <button className='reserve-button'
+                onClick={featureAlert}>Reserve</button>
             </div>
         </div>
         <hr className='rounded'/>
         <div className='reviews-printed'>
-            <h1>{reviewStats}</h1>
-            {spotReviews.length ? spotReviews.sort((a,b)=>Date.parse(b.createdAt)-Date.parse(a.createdAt))
+            <h1>{singleSpot.numReviews === 0 ? (<>
+            <i className="fa-solid fa-star"></i>
+            New!
+            </>) : (<>
+            <i className="fa-solid fa-star"></i>
+            {singleSpot.avgStarRating}
+             ●
+            {singleSpot.numReviews === 1 ? singleSpot.numReviews + ' Review'
+            :singleSpot.numReviews + ' Reviews'}
+         </>)}</h1>
+            {spotReviews.length ? spotReviews
+            .sort((a,b)=>Date.parse(b.createdAt)-Date.parse(a.createdAt))
             .map((rev, i) => (
                 <>
                 <h3 className='username-title'
-                key={'username'+i}>{rev.User.firstName}</h3>
+                key={'username-iter'+i}>{rev.User.firstName}</h3>
                 <h4 className='review-date'
-                key={'created'+i}>
+                key={'created-iter'+i}>
                     {new Intl.DateTimeFormat('en-GB', {
                     year: "numeric",
                     month: "long",
