4d3e037ca74e22acce84c6178eff783a3cafa12d
cludwell <cludwell@gmail.com>
2023-03-14 19:16:50 -0700

    think i made create spot correctly, well find out

--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -1,8 +1,15 @@
 export const POPULATE_SPOTS = 'spots/POPULATE'
 export const CREATE_SPOT = 'spots/CREATE'
+
 export const populateSpots = spotData =>{
     return {
         type: POPULATE_SPOTS,
+        spotData
+    }
+}
+export const createSpot = spotData =>{
+    return {
+        type: CREATE_SPOT,
         spotData
     }
 }
@@ -18,19 +25,30 @@ export const initialSpots = () => async dispatch => {
         return spotData;
     }
 };
+export const createSpotBackEnd = (spotData) => async dispatch => {
+    const response = await fetch('/api/spots', {"method": "POST"});
 
-const initialState = {
+    if (response.ok) {
+        const spotData = await response.json();
+        dispatch(createSpot(spotData));
+        console.log('THUNK', spotData)
+        return spotData;
+    }
+};
 
-}
+const initialState = {}
+
 //spots reducer
 export default function spotsReducer(state = initialState, action) {
     switch (action.type) {
         case POPULATE_SPOTS:
             const newState = {allSpots: {}, ...state }
-            // console.log('HERE IN REDUCER', action.spotData)
             action.spotData.Spots.forEach(s=> newState.allSpots[s.id] = s)
-            // console.log('NEW STATE', newState)
         return newState;
+        case CREATE_SPOT:
+            const beforeNewSpot = { ...state }
+            beforeNewSpot.allSpots[action.spotData.id] = action.spotData
+        return beforeNewSpot
         default:
         return state;
     }
