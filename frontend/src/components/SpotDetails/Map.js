d01d04b91097d7d82454b3695edf06d26e660e13
cludwell <cludwell@gmail.com>
2023-06-02 22:57:02 -0700

    google

---
+++ frontend/src/components/SpotDetails/Map.js
@@ -0,0 +1,9 @@
+
+import { GoogleMap, useLoadScript, Marker } from @react-google-maps/api
+
+export default function Map() {
+    const { isLoaded } = useLoadScr
+    return (
+        <div></div>
+    )
+}
