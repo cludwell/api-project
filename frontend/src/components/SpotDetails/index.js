b1697e6d2cee9ad7071dc27e2c574b2a77d52f9c
cludwell <cludwell@gmail.com>
2023-06-02 19:35:08 -0700

    accolades

--- frontend/src/components/SpotDetails/index.js
+++ frontend/src/components/SpotDetails/index.js
@@ -12,11 +12,16 @@ import PostReviewModal from '../PostReviewModal';
 import LoadingIcon from '../LoadingIcon';
 
 export default function SpotDetails() {
+    const today = new Date().toISOString().slice(0, -14)
+    const tomorrowData = new Date()
+    const day = tomorrowData.getDate() + 1
+    tomorrowData.setDate(day)
+    const tomorrow = tomorrowData.toISOString().slice(0, -14)
     const { spotId } = useParams()
     const dispatch = useDispatch();
     const [ hasLoaded, setHasLoaded ] = useState(false)
-    const [ checkin, setCheckin ] = useState()
-    const [ checkout, setCheckout ] = useState()
+    const [ checkin, setCheckin ] = useState(today)
+    const [ checkout, setCheckout ] = useState(tomorrow)
     const [ adults, setAdults ] = useState(1)
     const [ children, setChildren ] = useState(0)
     const [ infants, setInfants ] = useState(0)
@@ -55,9 +60,10 @@ export default function SpotDetails() {
     const spotReviews = useSelector(state => state.reviews)
     const user = useSelector(store => store.session.user)
     const averageRating = spotReviews.length >= 1 ? spotReviews.reduce((acc, ele) => acc + ele.stars, 0) / spotReviews.length : 'New!'
-    const stayDuration = checkout - checkin
-    const today = new Date()
-    const minDate = `${String(today.getMonth()).padStart(2, '0')}:${String(today.getDate()).padStart(2, '0')}:${today.getFullYear().toString()}`
+    const staySeconds = (Date.parse(checkout) - Date.parse(checkin))
+    const stayDuration = Math.floor(staySeconds/ (3600*24)) / 1000
+    const baseCost = (singleSpot.price * stayDuration).toFixed(0)
+    const serviceFee = (singleSpot.price * stayDuration * .1).toFixed(0)
     // const tomorrow = today +
     if (!hasLoaded) return <LoadingIcon />;
 
@@ -87,8 +93,31 @@ export default function SpotDetails() {
 
             </div>
             <h1 className='hosted-by'>Hosted by {singleSpot.Owner.firstName} {singleSpot.Owner.lastName}</h1>
+
             <div className='description'>
+            <div className='description-text'>
+            <div className='spot-accolades'>
+            <hr></hr>
+            <div className='dedicated-workspace spot-accolade'>
+                <div className='icon'><i class="fa-solid fa-house-laptop"></i></div>
+                <div className='accolade-text'>
+                    <span className='bold-text'>Dedicated Workspace</span>
+                    <span className='accolade-grey-text'>A room with wifi that's well-suited for working</span>
+                </div>
+            </div>
+            <div className='self-checkin spot-accolade'>
+                <div className='icon'><i class="fa-solid fa-door-open"></i></div>
+                <div className='accolade-text'>
+                    <span className='bold-text'>Dedicated Workspace</span>
+                    <span className='accolade-grey-text'>A room with wifi that's well-suited for working</span>
+                </div>
+            </div>
+
+            <hr></hr>
+            </div>
             <p className='description'>{singleSpot.description}</p>
+
+            </div>
             <div className='price-review-reserve'>
                 <div className='reserve-text'>
                 <span className='reserve-left'>
@@ -115,20 +144,39 @@ export default function SpotDetails() {
 
                 <div className='reserve-container checkin'>
                 <label className='reserve-checkin'>CHECK-IN</label>
-                <input className='reserve-start-date' type='date' value={checkin} on onChange={e => setCheckin(e.target.value)}
-                ></input>
+                <input className='reserve-start-date' type='date' value={checkin} on onChange={e => setCheckin(e.target.value)} min={today} ></input>
                 </div>
                 <div className='reserve-container checkout'>
                 <label className='reserve-checkout'>CHECKOUT</label>
-                <input className='reserve-end-date' type='date' value={checkout} onChange={e => setCheckout(e.target.value)}></input>
+                <input className='reserve-end-date' type='date' value={checkout} onChange={e => setCheckout(e.target.value)} min={tomorrow}></input>
                 </div>
 
                 </div>
 
                 <button className='reserve-button' >Reserve</button>
-                <p className='reserve-grey-text'>You won't be charged yet {minDate}</p>
-                <div className='reserve-breakdown'>{singleSpot.price} x {stayDuration}</div>
               </form>
+                <p className='reserve-grey-text'>You won't be charged yet </p>
+                <table className='reserve-breakdown'>
+                <tbody>
+                    <tr>
+                        <td className='reserve-item'>{singleSpot.price} x {stayDuration} nights</td>
+                        <td className='reserve-item-cost'>${baseCost}</td>
+                    </tr>
+                    <tr>
+                        <td className='reserve-item'>Cleaning fee</td>
+                        <td className='reserve-item-cost'>$20</td>
+                    </tr>
+                    <tr>
+                        <td className='reserve-item'>Scarebnb service fee</td>
+                        <td className='reserve-item-cost'>${serviceFee}</td>
+                    </tr>
+                </tbody>
+                </table>
+                <hr className='reserve-hr'></hr>
+                <div className='reserve-total-before-taxes'><span>Total Before Taxes</span> <span className='reserve-total-price'>${parseInt(baseCost) + parseInt(serviceFee) + 20}</span></div>
+
+
+
             </div>
         </div>
 
