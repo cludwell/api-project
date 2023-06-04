76c13a4e44f37f579f30ba2552070eda0c7e79f9
cludwell <cludwell@gmail.com>
2023-06-03 17:49:18 -0700

    looking good

--- frontend/src/components/LoadingIcon/index.js
+++ frontend/src/components/LoadingIcon/index.js
@@ -1,17 +1,11 @@
 import './LoadingIcon.css'
 import React from 'react'
-import Lottie from 'react-lottie';
-import spinningGhost from './SpinningGhost.json'
-
+import ghosts from '../../images/ghosts.gif'
 export default function LoadingIcon() {
-    const defaultOptions = {
-        loop: true,
-        autoplay: true,
-        animationData: spinningGhost,
-    };
+
     return (
         <div className='loading-container'>
-            <Lottie options={defaultOptions} id="loading-lottie" />
+            <img src={ghosts} alt='Loading...' />
         </div>
     )
 }
