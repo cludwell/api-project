fd54c16aab5d2b92de0bf5391f32880b7ad32c7c
cludwell <cludwell@gmail.com>
2023-06-09 15:52:59 -0700

    css

--- frontend/src/components/YourBookings/index.js
+++ frontend/src/components/YourBookings/index.js
@@ -25,27 +25,27 @@ export default function YourBookings() {
             <hr></hr>
             {bookings.length ? bookings.map((book, i) => (
             <div className={i % 2 === 0 ?'booking' : 'grey-booking booking'} key={`container${i}`}>
-            <div className='booking-row'>
-            <div className='booking-text'>
-            <div className='booking-times'>
-            <div className='booking-checkin'>
-                <div className='booking-checkin-deets checkin-day'>{new Date(book.startDate).toString().split(' ')[0]}</div>
-                <div className='booking-checkin-deets checkin-month-day'>{new Date(book.startDate).toString().slice(3, 11)}</div>
-                <div className='booking-checkin-deets checkin-year'>{new Date(book.startDate).toString().slice(11, 15)}</div>
+            <div className='booking-row' key={`bookingrow${i}`}>
+            <div className='booking-text' key={`bookingtext${i}`}>
+            <div className='booking-times' key={`bookingtimes${i}`}>
+            <div className='booking-checkin' key={`bookingcheckin${i}`}>
+                <div className='booking-checkin-deets checkin-day' key={`bookingcheckindeets${i}`}>{new Date(book.startDate).toString().split(' ')[0]}</div>
+                <div className='booking-checkin-deets checkin-month-day' key={`bookingcheckindeetsmonthday${i}`}>{new Date(book.startDate).toString().slice(3, 11)}</div>
+                <div className='booking-checkin-deets checkin-year' key={`bookingcheckindeetsyear${i}`}>{new Date(book.startDate).toString().slice(11, 15)}</div>
             </div>
-            <div className='booking-checkout'>
-                <div className='booking-checkout-deets checkout-day'>{new Date(book.endDate).toString().split(' ')[0]}</div>
-               <div className='booking-checkout-deets checkout-month-day'>{new Date(book.endDate).toString().slice(3, 11)}</div>
-               <div className='booking-checkout-deets checkout-year'>{new Date(book.endDate).toString().slice(11, 15)}</div>
+            <div className='booking-checkout' key={`bookingcheckout${i}`}>
+                <div className='booking-checkout-deets checkout-day' key={`bookingcheckoutday${i}`}>{new Date(book.endDate).toString().split(' ')[0]}</div>
+               <div className='booking-checkout-deets checkout-month-day' key={`bookingcheckoutdeetsmonth${i}`}>{new Date(book.endDate).toString().slice(3, 11)}</div>
+               <div className='booking-checkout-deets checkout-year' key={`bookingcheckoutyear${i}`}>{new Date(book.endDate).toString().slice(11, 15)}</div>
             </div>
 
             </div>
-            <div className='booking-address'>
-            <div className='booking-spot-name' key={`name${i}`}>{book.Spot.name}</div>
-            <div>{book.Spot.address} </div>
-            <div>{book.Spot.city} {book.Spot.state}, {book.Spot.country} </div>
+            <div className='booking-address' key={`bookingaddress${i}`}>
+            <div className='booking-spot-name' key={`bookingname${i}`}>{book.Spot.name}</div>
+            <div key={`spotaddress${i}`}>{book.Spot.address} </div>
+            <div key={`spotcitystate${i}`}>{book.Spot.city} {book.Spot.state}, {book.Spot.country} </div>
             </div>
-                </div>
+            </div>
             <BookingMap spot={book.Spot} key={`map${i}`}/>
 
 
